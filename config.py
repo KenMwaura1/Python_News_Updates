c65d65688947eeef8bdad57d54ee1fca10538d06
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-11 02:41:07 +0300

    Updated the config with Api links and created the requests file

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ class Config:
     """
     Base class for general configuration settings
     """
-    NEWS_API_BASE_URL = ''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+    NEWS_ARTICLES_APL_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
+    NEWS_SOURCE_ARTICLES_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey='
 
 
 class DevConfig(Config):
