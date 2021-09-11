5d7db64318e6587cc9570d072ca404933a9554d5
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-11 15:57:43 +0300

    Updated the requests file to get articles

--- app/news_requests.py
+++ app/news_requests.py
@@ -1,6 +1,6 @@
 import requests
 
-from .models import Articles, NewsSources
+from models import Articles, NewsSources
 
 news_sources_url = None
 news_articles_url = None
@@ -19,3 +19,47 @@ def request_config(app):
     news_articles_url = app.config.get('NEWS_ARTICLES_APL_URL')
     news_base_url = app.config.get('NEWS_API_BASE_URL')
     api_key = app.config.get('api_key')
+
+
+def process_articles_response(list_articles):
+    """
+    function to process articles
+    :param list_articles:
+    :return: list of articles
+    """
+    list_of_articles = []
+    for article in list_articles:
+        source = article.get("source")
+        author = article.get("author")
+        description = article.get("description")
+        title = article.get("title")
+        url = article.get("url")
+        url_to_image = article.get("urlToImage")
+        published_at = article.get("publishedAt")
+
+        new_article = Articles(source, author, title,
+                               description, url, url_to_image, published_at)
+        list_of_articles.append(new_article)
+
+    return list_of_articles
+
+
+def get_articles(article):
+    """
+    function to get articles from the News Api
+    :param article:
+    :return: list of articles
+    """
+    articles_url = news_articles_url.format(article, api_key)
+    with requests.get(articles_url) as url:
+        articles_response = url.json()
+        print(articles_response)
+
+        list_of_articles = None
+        if articles_response['results']:
+            list_of_articles = process_articles_response(articles_response['results'])
+
+    return list_of_articles
+
+
+def
