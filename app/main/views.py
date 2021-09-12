4ddf906a0f0fd43c932f63d590d16a4b2348d506
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-12 05:57:54 +0300

    Added code to search and display articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from . import main
-from ..news_requests import get_articles, get_news_sources, get_news_sources_articles, get_all_sources
+from ..news_requests import get_articles, get_news_sources, get_news_sources_articles, get_all_sources, search_articles
 
 
 @main.route('/')
@@ -14,6 +14,10 @@ def index():
     business = get_news_sources('business')
     tech = get_news_sources('technology')
     sports = get_news_sources('sports')
+
+    search = request.args.get('article-query')
+    if search:
+        return redirect(url_for('main.search_article', article_search=search))
 
     return render_template('index.html', general=general_news, business=business, tech=tech, sports=sports)
 
@@ -29,6 +33,7 @@ def news_source_articles(id):
     all_source_articles = get_news_sources_articles(id)
     return render_template("article-sources.html", source=source, all_source_articles=all_source_articles)
 
+
 @main.route('/News-Articles')
 def news_articles():
     """
@@ -38,7 +43,8 @@ def news_articles():
     tech = get_articles('technology')
     science = get_articles('science')
     health = get_articles('health')
-    return render_template("articles.html",tech=tech,science=science,health=health)
+    return render_template("articles.html", tech=tech, science=science, health=health)
+
 
 @main.route('/News-Sources')
 def news_sources():
@@ -48,3 +54,18 @@ def news_sources():
     """
     all_news_sources = get_all_sources()
     return render_template("news_sources.html", all_news_sources=all_news_sources)
+
+
+@main.route('/search/<string:article_search>')
+def search_article(article_search: str):
+    """
+    function that returns search results
+    :param article_search:
+    :return:
+    """
+    search_term = article_search.split(" ")
+    search_term_format = "+".join(search_term)
+    search_results = search_articles(search_term_format)
+    for s in search_results:
+        print(s.title)
+    return render_template("search_results.html", search_results=search_results)
